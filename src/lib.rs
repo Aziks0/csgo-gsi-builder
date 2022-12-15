d797720161e07d584fb922ab6f965f8f9b510fac
Benjamin DIDIER <contact@aziks.aleeas.com>
2022-12-15 21:33:45 +0100

    feat: add config installer

--- src/lib.rs
+++ src/lib.rs
@@ -30,4 +30,27 @@ impl Builder {
     pub fn output(&self) -> String {
         self.output.clone()
     }
+
+    pub fn install<P: Into<PathBuf>>(
+        &self,
+        folder_path: P,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        assert!(
+            !self.output.is_empty(),
+            "config should be build before installing"
+        );
+
+        let mut path: PathBuf = folder_path.into();
+
+        let folder_path = path.as_path();
+        let metadata = fs::metadata(folder_path)?;
+        if !metadata.is_dir() {
+            return Err(format!("{} is not a directory", folder_path.to_string_lossy()).into());
+        }
+
+        path.push(&format!("gamestate_integration_{}.cfg", self.config.name));
+        fs::write(path, self.output.as_bytes())?;
+
+        Ok(())
+    }
 }
